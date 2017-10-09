3c2c39488f7046d8b5d50736bac0bb51f683f3ef
Alexander Keating <atk315315@gmail.com>
2017-10-09 08:26:38 -0400

    Added question 2
    Added a check_permutatios function and tests. As well
    as tests for question 3.

---
+++ questions/arrays/questions.py
@@ -0,0 +1,57 @@
+from data_structures.hash_table import HashTable
+
+def string_is_unique(string):
+    """
+    Question 1: Implement an algoithm to determine if a string has all unique 
+    characters. What if you cannot use additional data structures.
+
+    Big O: 
+        Time: O(n)
+        Space: O(1)
+    
+    Possible solution in python is to use a set and compare the length of
+    the set to the length of the original string.
+    """
+    unique_char = []
+    for char in string:
+        if char in unique_char:
+            return False
+        unique_char.append(char)
+    return True
+
+def check_permutations(s_one, s_two):
+    """
+    Question 2: Given two strings, write a method to decide if one is a permutation
+    of the other.
+    """
+    if len(s_one) != len(s_two):
+        return False
+
+    h_table = {} 
+    for index, char in enumerate(s_two):
+        h_table[char] = index
+
+    for char in set(s_one):
+        if not char in list(h_table.keys()):
+            return False
+        del h_table[char]
+    return True
+
+def urlify(string):
+    """
+    Question 3: Write a method to replace all spaces in a string with '%20'. You may
+    assume that the string has suffcient space at the end to hold the additional characters,
+    and that you are given the "true" length of the string.
+
+    In python I can use the replace method. string.replace(' ', '%20')
+    """
+    pass
+
+def palindrome_permutation(string):
+   """
+   Question 4: Given a string, write a function to check if it is a permutation of a palindrome.
+   A palindrome is a word or phrase that is the same forwards and backwords. A permutation is 
+   a rearrangement of letters. The palindrome does not need to be limited to a dictionary of 
+   words.
+   """
+   pass
---
+++ questions/arrays/test_questions.py
@@ -0,0 +1,63 @@
+import unittest
+
+from questions.arrays.questions import (check_permutations,
+                                        string_is_unique,
+                                        urlify)
+
+class ArrayQuestionsTest(unittest.TestCase):
+
+    def test_string_is_unique_blank(self):
+        string = ''
+        is_unique = string_is_unique(string)
+        self.assertEqual(True, is_unique)
+
+    def test_string_is_not_unique(self):
+        string = 'alabama'
+        is_unique = string_is_unique(string)
+        self.assertEqual(False, is_unique)
+
+    def test_string_is_unique(self):
+        string = 'abcdefghijklmnopqrstuvwxyz'
+        is_unique = string_is_unique(string)
+        self.assertEqual(True, is_unique)
+
+    def test_check_permutations_blanks(self):
+        string_one = ''
+        string_two = ''
+        is_permutation = check_permutations(string_one, string_two)
+        self.assertEqual(True, is_permutation)
+
+    def test_check_permutations_not_permutaion(self):
+        string_one = 'alex'
+        string_two = 'alax'
+        is_permutation = check_permutations(string_one, string_two)
+        self.assertEqual(False, is_permutation)
+
+    def test_check_permutations_different_length(self):
+        string_one = 'pete'
+        string_two = 'pet'
+        is_permutation = check_permutations(string_one, string_two)
+        is_permutation_reverse = check_permutations(string_two, string_one)
+        self.assertEqual(False, is_permutation)
+        self.assertEqual(False, is_permutation_reverse)
+
+    def test_check_permutations_permutation(self):
+        string_one = 'tab'
+        string_two = 'bat'
+        is_permutation = check_permutations(string_one, string_two)
+        self.assertEqual(True, is_permutation)
+
+    def test_urlify_none(self):
+        string = None
+        new_string = urlify(string)
+        self.assertEqual(None, new_string)
+
+    def test_urlify_no_spaces(self):
+        string = 'albert'
+        new_string = urlify(string)
+        self.assertEqual(string, new_string)
+
+    def test_urlify_with_spaces(self):
+        string = 'He was a great  man! '
+        new_string = urlify(string)
+        self.assertEqual(new_string, 'He%20was%20a%20great%20man!%20')
