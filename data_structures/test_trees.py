ee049e6bc4798ffb1757940f20b0514761a6bb41
Alexander Keating <atk315315@gmail.com>
2018-01-07 10:44:25 -0500

    Added binary search tree

--- data_structures/test_trees.py
+++ data_structures/test_trees.py
@@ -1,6 +1,14 @@
 import unittest
 
-from data_structures.tree import TreeNode
+from data_structures.tree import TreeNode, BinarySearchTree
+
+
+def build_binary_search_tree(keys_values):
+    b_tree = BinarySearchTree()
+    for key, value in keys_values:
+        b_tree.put(key, value)
+
+    return b_tree
 
 
 class TreeNodeTest(unittest.TestCase):
@@ -9,8 +17,390 @@ class TreeNodeTest(unittest.TestCase):
         pass
 
     def test_tree_node_instatiate(self):
-        tree = TreeNode(data='test', children=[TreeNode(data='test2'),
-                                               TreeNode(data='test3')])
-        self.assertEqual(tree.data, 'test')
-        self.assertEqual(tree.children[0].data, 'test2')
-        self.assertEqual(tree.children[1].data, 'test3')
+        right_node = TreeNode(key='test2', value=1)
+        left_node = TreeNode(key='test3', value=2)
+        tree = TreeNode(key='test', value=0, left_child=left_node,
+                        right_child=right_node)
+        self.assertEqual(tree.value, 0)
+        self.assertEqual(tree.right_child.value, 1)
+        self.assertEqual(tree.left_child.value, 2)
+
+    def test_has_right_child(self):
+        right_node = TreeNode(key='test2', value=1)
+        left_node = TreeNode(key='test3', value=2)
+        tree = TreeNode(key='test', value=0, left_child=left_node,
+                        right_child=right_node)
+
+        self.assertEqual(tree.has_right_child, True)
+    
+    def test_has_right_child_false(self):
+        left_node = TreeNode(key='test3', value=2)
+        tree = TreeNode(key='test', value=0, left_child=left_node,
+                        right_child=None)
+
+        self.assertEqual(tree.has_right_child, False)
+    
+    def test_has_left_child(self):
+        left_node = TreeNode(key='test3', value=2)
+        tree = TreeNode(key='test', value=0, left_child=left_node,
+                        right_child=None)
+
+        self.assertEqual(tree.has_left_child, True)
+    
+    def test_has_left_child_false(self):
+        tree = TreeNode(key='test', value=0, left_child=None,
+                        right_child=None)
+
+        self.assertEqual(tree.has_right_child, False)
+
+    def test_is_left_child(self):
+        left_node = TreeNode(key='test1', value=1) 
+        parent = TreeNode(key='test', value=0, left_child=left_node,
+                          right_child=None)
+        left_node.parent = parent
+
+        self.assertEqual(left_node.is_left_child, True)
+
+    def test_is_left_child_false(self):
+        left_node = TreeNode(key='test1', value=1) 
+
+        self.assertEqual(left_node.is_left_child, False)
+    
+    def test_is_right_child(self):
+        right_node = TreeNode(key='test1', value=1)
+        parent = TreeNode(key='test', value=0, left_child=None,
+                          right_child=right_node)
+        right_node.parent = parent
+
+        self.assertEqual(right_node.is_right_child, True)
+
+    def test_is_right_child_false(self):
+        right_node = TreeNode(key='test2', value=1)
+
+        self.assertEqual(right_node.is_right_child, False)
+    
+    def test_is_root(self):
+        tree = TreeNode(key='test1', value=1)
+
+        self.assertEqual(tree.is_root, True)
+
+    def test_is_root_false(self):
+        parent = TreeNode(key='test', value=0)
+        child = TreeNode(key='test1', value=1, parent=parent)
+
+        self.assertEqual(child.is_root, False)
+
+    def test_is_leaf(self):
+        leaf = TreeNode(key='test', value=1)
+
+        self.assertEqual(leaf.is_leaf, True)
+
+    def test_is_leaf_false_left(self):
+        child = TreeNode(key='test1', value=1)
+        parent = TreeNode(key='test', value=0, left_child=child)
+
+        self.assertEqual(parent.is_leaf, False)
+
+    def test_is_leaf_false_right(self):
+        child = TreeNode(key='test1', value=1)
+        parent = TreeNode(key='test', value=0, right_child=child)
+
+        self.assertEqual(parent.is_leaf, False)
+
+    def test_has_any_children_right(self):
+        right_child = TreeNode(key='test1', value=1)
+        parent = TreeNode(key='test', value=0, right_child=right_child)
+
+        self.assertEqual(parent.has_any_children, True)
+
+    def test_has_any_children_left(self):
+        left_child = TreeNode(key='test1', value=1)
+        parent = TreeNode(key='test', value=0, left_child=left_child)
+
+        self.assertEqual(parent.has_any_children, True)
+
+    def test_has_any_children_both(self):
+        right_child = TreeNode(key='test1', value=1)
+        left_child = TreeNode(key='test2', value=2)
+        parent = TreeNode(key='test', value=0, right_child=right_child,
+                          left_child=left_child)
+
+        self.assertEqual(parent.has_any_children, True)
+
+    def test_has_any_children_false(self):
+        parent = TreeNode(key='test', value=0)
+
+        self.assertEqual(parent.has_any_children, False)
+
+    def test_has_both_children(self):
+        right_child = TreeNode(key='test1', value=1)
+        left_child = TreeNode(key='test2', value=2)
+        parent = TreeNode(key='test', value=0, right_child=right_child,
+                          left_child=left_child)
+
+        self.assertEqual(parent.has_both_children, True)
+
+    def test_has_both_children_both(self):
+        parent = TreeNode(key='test', value=0)
+
+        self.assertEqual(parent.has_any_children, False)
+    
+    def test_replace_node_data_no_children(self):
+        tree = TreeNode(key='test', value=0)
+        tree.replace_node_data(value=1, key='new_value')
+
+        self.assertEqual(tree.value, 1)
+        self.assertEqual(tree.key, 'new_value')
+        self.assertEqual(tree.right_child, None)
+        self.assertEqual(tree.left_child, None)
+
+    def test_replace_node_data_left_child(self):
+        left_child = TreeNode(key='left', value=2)
+        tree = TreeNode(key='test', value=0)
+        tree.replace_node_data(value=1, key='new_value',
+                               left_child=left_child)
+
+        self.assertEqual(tree.value, 1)
+        self.assertEqual(tree.key, 'new_value')
+        self.assertEqual(tree.left_child, left_child)
+        self.assertEqual(tree.right_child, None)
+        self.assertEqual(left_child.parent, tree)
+
+    def test_replace_node_data_right_child(self):
+        right_child = TreeNode(key='right', value=2)
+        tree = TreeNode(key='test', value=0)
+        tree.replace_node_data(value=1, key='new_value',
+                               right_child=right_child)
+
+        self.assertEqual(tree.value, 1)
+        self.assertEqual(tree.key, 'new_value')
+        self.assertEqual(tree.right_child, right_child)
+        self.assertEqual(tree.left_child, None)
+        self.assertEqual(right_child.parent, tree)
+
+    def test_replace_node_data_both_children(self):
+        right_child = TreeNode(key='right', value=2)
+        left_child = TreeNode(key='left', value=0)
+        tree = TreeNode(key='test', value=0)
+        tree.replace_node_data(value=1, key='new_value',
+                               right_child=right_child,
+                               left_child=left_child)
+
+        self.assertEqual(tree.value, 1)
+        self.assertEqual(tree.key, 'new_value')
+        self.assertEqual(tree.right_child, right_child)
+        self.assertEqual(tree.left_child, left_child)
+        self.assertEqual(left_child.parent, tree)
+        self.assertEqual(right_child.parent, tree)
+
+    def test_find_sucessor_in_right_subtree(self):
+        nodes = [(17, 'root'), (5, 'left_1'), (2, 'left_2'), (11, 'right_2'),
+                 (9, 'left_3'), (16, 'right_3'), (7, 'left_4'), (8, 'left_5'),
+                 (35, 'right_1')]
+        tree = build_binary_search_tree(nodes)
+        node = tree._get(5, tree.root)
+        successor = node.find_successor()
+        self.assertEqual(successor.key, 7)
+        self.assertEqual(successor.value, 'left_4')
+
+    def test_right_child_has_no_right_child_successor(self):
+        nodes = [(17, 'root'), (10, 'left_1'), (35, 'right_1'),
+                 (30, 'left_2'), (20, 'left_3'), (33, 'right_3'),
+                 (32, 'left_4')]
+        tree = build_binary_search_tree(nodes)
+        node = tree._get(33, tree.root)
+        successor = node.find_successor()
+        self.assertEqual(successor.key, 35)
+        self.assertEqual(successor.value, 'right_1')
+
+    def test_no_right_child_is_left_child_successor(self):
+        nodes = [(17, 'root'), (10, 'left_1'), (35, 'right_1'),
+                 (20, 'left_2'), (37, 'left_3'), (42, 'right_2'),
+                 (50, 'right_3'), (55, 'right_4')]
+        tree = build_binary_search_tree(nodes)
+        node = tree._get(37, tree.root)
+        successor = node.find_successor()
+        self.assertEqual(successor.key, 42)
+        self.assertEqual(successor.value, 'right_2')
+
+    def test_find_min_has_left_leaf(self):
+        left_node_2 = TreeNode(key='left_node_2', value=-2)
+        left_node = TreeNode(key='left_1', value=-1, left_child=left_node_2)
+        right_node = TreeNode(key='right_1', value=1)
+        root_node = TreeNode(key='root', value=0,
+                             right_child=right_node,
+                             left_child=left_node)
+        right_node.parent = root_node
+        left_node.parent = root_node
+        left_node_2.parent = left_node
+
+        min_node = root_node.find_min()
+        self.assertEqual(min_node, left_node_2)
+
+    def test_find_min_has_no_left_leaf(self):
+        right_node = TreeNode(key='right_1', value=1)
+        root_node = TreeNode(key='root', value=0,
+                             right_child=right_node,
+                             left_child=None)
+        right_node.parent = root_node
+
+        min_node = root_node.find_min()
+        self.assertEqual(min_node, root_node)
+
+    def test_splice_out_is_leaf_left_child(self):
+        left_node = TreeNode(key=1, value='left')
+        right_node = TreeNode(key=12, value='right')
+        parent = TreeNode(key=10, value='root', left_child=left_node,
+                          right_child=right_node)
+        left_node.parent = parent
+        right_node.parent = parent
+        left_node.splice_out()
+        self.assertEqual(left_node.parent.left_child, None)
+        self.assertEqual(right_node.parent.right_child, right_node)
+
+    def test_splice_put_is_leaf_right_child(self):
+        right_node = TreeNode(key=12, value='right')
+        left_node = TreeNode(key=1, value='left')
+        parent = TreeNode(key=10, value='root', right_child=right_node,
+                          left_child=left_node)
+        right_node.parent = parent
+        left_node.parent = parent
+        right_node.splice_out()
+        self.assertEqual(right_node.parent.right_child, None)
+        self.assertEqual(left_node.parent.left_child, left_node)
+
+    def test_splice_out_has_left_child_is_left_child(self):
+        left_grandchild = TreeNode(key=-1, value='left_grandchild')
+        left_node = TreeNode(key=1, value='left', left_child=left_grandchild)
+        parent = TreeNode(key=10, value='root', left_child=left_node)
+        left_node.parent = parent
+        left_node.splice_out()
+        self.assertEqual(parent.left_child, left_grandchild)
+
+    def test_splice_out_has_left_child_is_right_child(self):
+        left_grandchild = TreeNode(key=-1, value='left_grandchild')
+        right_node = TreeNode(key=1, value='left', left_child=left_grandchild)
+        parent = TreeNode(key=10, value='root', right_child=right_node)
+        right_node.parent = parent
+        right_node.splice_out()
+        self.assertEqual(parent.right_child, left_grandchild)
+
+    def test_splice_out_has_left_child(self):
+        left_grandchild = TreeNode(key=-1, value='left_grandchild')
+        right_node = TreeNode(key=1, value='left', left_child=left_grandchild)
+        parent = TreeNode(key=10, value='root', right_child=right_node)
+        right_node.parent = parent
+        right_node.splice_out()
+        self.assertEqual(left_grandchild.parent, parent)
+   
+
+    def test_splice_out_has_right_child_is_left_child(self):
+        left_grandchild = TreeNode(key=-1, value='left_grandchild')
+        right_node = TreeNode(key=1, value='left', left_child=left_grandchild)
+        parent = TreeNode(key=10, value='root', right_child=right_node)
+        right_node.parent = parent
+        right_node.splice_out()
+        self.assertEqual(parent.right_child, left_grandchild)
+
+    def test_splice_out_has_right_child_is_right_child(self):
+        right_grandchild = TreeNode(key=-1, value='left_grandchild')
+        right_node = TreeNode(key=1, value='left', right_child=right_grandchild)
+        parent = TreeNode(key=10, value='root', right_child=right_node)
+        right_node.parent = parent
+        right_node.splice_out()
+        self.assertEqual(parent.right_child, right_grandchild)
+
+    def test_splice_out_has_right_child(self):
+        right_grandchild = TreeNode(key=-1, value='left_grandchild')
+        right_node = TreeNode(key=1, value='left', right_child=right_grandchild)
+        parent = TreeNode(key=10, value='root', right_child=right_node)
+        right_node.parent = parent
+        right_node.splice_out()
+        self.assertEqual(right_grandchild.parent, parent)
+
+
+class BinarySearchTreeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_put_is_root(self):
+        pass
+
+    def test_put_not_root(self):
+        pass
+
+    def test__insert_key_greater_than_current_node_left_child(self):
+        pass
+
+    def test__insert_key_greater_than_current_node_right_child(self):
+        pass
+
+    def test__insert_key_less_than_current_node_right_child(self):
+        pass
+
+    def test__insert_key_less_than_current_node_right_child(self):
+        pass
+
+    def test_get_is_root(self):
+        pass
+
+    def test_is_root_response(self):
+        pass
+
+    def test_is_root_response_none(self):
+        pass
+
+    def test_is_not_root(self):
+        pass
+
+    def test__get_no_current_node(self):
+        pass
+
+    def test__get_current_node_key_equal(self):
+        pass
+
+    def test__get_current_node_key_greater_than(self):
+        pass
+
+    def test__get_all_other_cases(self):
+        pass
+
+    def test_delete_size_greater_than_one_node_to_remove(self):
+        pass
+
+    def test_delete_size_greater_than_one_no_node_to_remove(self):
+        pass
+
+    def test_delete_size_equal_to_one_and_key_is_root(self):
+        pass
+
+    def test_delete_size_is_less_than_one(self):
+        pass
+
+    def test_remove_is_leaf_parent_has_left_child(self):
+        pass
+
+    def test_remove_is_leaf_parent_has_right_child(self):
+        pass
+
+    def test_remove_has_both_children(self):
+        pass
+
+    def test_remove_has_left_child_is_left_child(self):
+        pass
+
+    def test_remove_has_left_child_is_right_child(self):
+        pass
+
+    def test_remove_has_left_child_has_not_parent(self):
+        pass
+
+    def test_remove_has_right_child_is_right_child(self):
+        pass
+
+    def test_remove_has_right_child_is_left_child(self):
+        pass
+
+    def test_remove_has_right_child_is_not_parent(self):
+        pass
--- data_structures/tree.py
+++ data_structures/tree.py
@@ -1,8 +1,219 @@
 class TreeNode(object):
 
-    def __init__(self, data, children=None):
-        self.data = data
-        self.children = children
+    def __init__(self, key, value, left_child=None, right_child=None, parent=None):
+        self.value = value
+        self.key = key
+        self.left_child = left_child
+        self.right_child = right_child
+        self.parent = parent
 
     def __repr__(self):
-        return f'<TreeNode {self.data}>'
+        return f'<TreeNode key={self.key} value={self.value}>'
+    
+    @property
+    def has_right_child(self):
+        return self.right_child is not None
+    
+    @property
+    def has_left_child(self):
+        return self.left_child is not None
+
+    @property
+    def is_left_child(self):
+        return self.parent is not None and self.parent.left_child == self
+
+    @property
+    def is_right_child(self):
+        return self.parent is not None and self.parent.right_child == self
+
+    @property
+    def is_root(self):
+        return not self.parent
+
+    @property
+    def is_leaf(self):
+        return not self.has_any_children
+
+    @property
+    def has_any_children(self):
+        return self.has_left_child or self.has_right_child
+
+    @property
+    def has_both_children(self):
+        return self.has_left_child and self.has_right_child
+
+    def replace_node_data(self, value, key, left_child=None, right_child=None):
+        self.key = key
+        self.value = value
+        self.left_child = left_child
+        self.right_child = right_child
+
+        if self.has_left_child:
+            self.left_child.parent = self
+        if self.has_right_child:
+            self.right_child.parent = self
+
+    def find_successor(self):
+        successor = None
+        if self.has_right_child:
+            successor = self.right_child.find_min()
+        else:
+            if self.parent:
+                if self.is_left_child:
+                    successor = self.parent
+                else:
+                    self.parent.right_child = None
+                    successor = self.parent.find_successor()
+                    self.parent.right_cild = self
+        return successor
+
+    def find_min(self):
+        current = self
+        while current.has_left_child:
+            current = current.left_child
+        return current
+
+    def splice_out(self):
+        """
+        Removes successor node.
+        """
+        if self.is_leaf:
+            if self.is_left_child:
+                self.parent.left_child = None
+            else:
+                self.parent.right_child = None
+        elif self.has_any_children:
+            if self.has_left_child:
+                if self.is_left_child:
+                    self.parent.left_child = self.left_child
+                else:
+                    self.parent.right_child = self.left_child
+                self.left_child.parent = self.parent
+            else:
+                if self.is_left_child:
+                    self.parent.left_child = self.right_child
+                else:
+                    self.parent.right_child = self.right_child
+                self.right_child.parent = self.parent
+
+
+class BinarySearchTree(object):
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+
+    def __getitem__(self, key, value):
+        self.get(key)
+
+    def __contains__(self, key):
+        has_key = self.get(key)
+        if has_key:
+            return True
+        else:
+            return False
+
+    def __delitem__(self, key):
+        self.delete(key)
+
+    def put(self, key, value):
+        if self.root:
+            self._insert(self.root, key, value)
+        else:
+            self.root = TreeNode(key, value)
+
+    def _insert(self, current_node, key, value):
+        """
+        Does not handle duplicate keys.
+        """
+        if key < current_node.key:
+            if current_node.has_left_child:
+                self._insert(current_node.left_child, key, value)
+            else:
+                current_node.left_child = TreeNode(key=key, value=value, parent=current_node)
+        else:
+            if current_node.has_right_child:
+                self._insert(current_node.right_child, key, value)
+            else:
+                current_node.right_child = TreeNode(key=key, value=value, parent=current_node)
+
+    def get(self, key):
+        if self.root:
+            response = self._get(key, self.root)
+            if response:
+                return response.value
+            else:
+                return None
+        else:
+            return None
+
+    def _get(self, key, current_node):
+        if not current_node:
+            return None
+        elif current_node.key == key:
+            return current_node
+        elif key < current_node.key:
+            return self._get(key, current_node.left_child)
+        else:
+            return self._get(key, current_node.right_child)
+
+    def delete(self, key):
+        if self.size > 1:
+            node_to_remove = self.get(key)
+            if node_to_remove:
+                self.remove(node_to_remove)
+                self.size -= 1
+            else:
+                raise KeyError("KeyError: Key not in BinaryTree unable to delete.")
+        elif self.size == 1 and self.root.key == key:
+            self.root = None
+            self.size -= 1
+        else:
+            raise KeyError("KeyError: Key not in BinaryTree unable to delete.")
+    
+    def remove(current_node):
+        if current_node.is_leaf:
+            if current_node == current_node.parent.has_left_child:
+                current_node.parent.has_left_child = None
+            else:
+                current_node.parent.has_right_child = None
+        elif current_node.has_both_children:
+            successor = current_node.find_successor()
+            successor.splice_out()
+            current_node.key = successor.key
+            current_node.value = successor.value
+        else:
+            if current_node.has_left_child:
+                if current_node.is_left_child:
+                    current_node.left_child.parent == current_node.parent
+                elif current_node.is_right_child:
+                    current_node.left_child.parent = current_node.parent
+                    current_node.parent.right_child = current_node.left_child
+                else:
+                    current_node.replace_node_data(current_node.left_child.value,
+                                                   current_node.left_child.key,
+                                                   current_node.left_child.left_child,
+                                                   current_node.left_child.right_child)
+            else:
+                if current_node.is_right_child:
+                    current_node.right_child.parent == current_node.parent
+                elif current_node.is_left_child:
+                    current_node.right_child.parent = current_node.parent
+                    current_node.parent.right_child = current_node.right_child
+                else:
+                    current_node.replace_node_data(current_node.right_child.value,
+                                                   current_node.right_child.key,
+                                                   current_node.right_child.left_child,
+                                                   current_node.right_child.left_child)
+
+
+
